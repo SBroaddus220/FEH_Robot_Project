cef40e82dd321cc685f512bec8ed07f078237169
SBroaddus220 <Steven.Broaddus220@gmail.com>
2022-02-21 00:44:31 -0500

    Initialize main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,40 @@
+/*********************************************/
+/*      Team A3 FEH Robot Project Code       */
+/*            OSU FEH Spring 2022            */
+/*                                           */
+/*      Steven Broaddus, Conolly Burgess     */
+/*        Joseph Richmond, Jake Chang        */
+/*                                           */
+/*            Updated 2/20/2022              */
+/*       Uses Doxygen for documentation      */
+/*********************************************/
+
+// Include preprocessor directives
 #include <FEHLCD.h>
+#include <FEHIO.h>
+#include <FEHUtility.h>
+#include <FEHMotor.h>
+#include <FEHRPS.h>
+
+/*
+// Declarations for encoders/motors
+DigitalEncoder right_encoder(FEHIO::P0_0);
+DigitalEncoder left_encoder(FEHIO::P0_1);
+FEHMotor right_motor(FEHMotor::Motor0,9.0);
+FEHMotor left_motor(FEHMotor::Motor1,9.0);
+*/
+
+/*
+// Declaration for CdS cell sensorsad 
+AnalogInputPin CdS_cell(FEHIO::P0_0);
+*/
 
 int main(void)
 {
-    LCD.WriteLine("Hello World!");
-	return 0;
+    //Initialize the screen
+    LCD.Clear(BLACK);
+    LCD.SetFontColor(WHITE);
+
+    return 0;
 }
+
